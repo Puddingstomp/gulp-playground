eec472b29f070d8e3139f79292ae61a3dba5bffa
Jonathan Grubb <joncgrubb@gmail.com>
2017-09-20 15:44:35 -0400

    automated sass with gulp with cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var clean = require('gulp-clean');
 
 //this is a simple sample task
 gulp.task('task-name', function() {
@@ -9,14 +10,33 @@ gulp.task('task-name', function() {
 });
 
 //compile any .scss files in /app/scss and save to dist/css
-gulp.task('sass', function() {
+gulp.task('sass', ['cleanup'], function() {
 	return gulp.src('app/scss/*.scss') //source file(s), *.scss is a glob
 	.pipe(sass()) //run sass
 	.pipe(gulp.dest('app/css')); //output file(s)
 });
 
-gulp.task('concat', function() {
+//concatenate files into one and clean up old files
+gulp.task('concat', ['sass'], function() {
 	return gulp.src('app/css/*.css')
 	.pipe(concat('all.css'))
 	.pipe(gulp.dest('dist/css'))
 });
+
+
+//cleanup task
+gulp.task('cleanup', function() {
+	return gulp.src('app/css/*.css', {read: false})
+	.pipe(clean());
+})
+
+//chain sass and concat together
+
+gulp.task('doit', ['cleanup', 'sass', 'concat']);
+
+//watch scss folder and runauto when files change
+
+gulp.task('watch', function() {
+	gulp.watch('app/scss/*.scss', ['doit']);
+})
+
