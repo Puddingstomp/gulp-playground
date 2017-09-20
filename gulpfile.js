fe146653616e09ea61897f0a42ac00db670ca268
Jonathan Grubb <joncgrubb@gmail.com>
2017-09-20 15:02:11 -0400

    added sass and concat functions to gulp js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
+//this is a simple sample task
 gulp.task('task-name', function() {
 //Task stuff goes here
 	console.log('Hello!');
-})
+});
+
+//compile any .scss files in /app/scss and save to dist/css
+gulp.task('sass', function() {
+	return gulp.src('app/scss/*.scss') //source file(s), *.scss is a glob
+	.pipe(sass()) //run sass
+	.pipe(gulp.dest('app/css')); //output file(s)
+});
+
+gulp.task('concat', function() {
+	return gulp.src('app/css/*.css')
+	.pipe(concat('all.css'))
+	.pipe(gulp.dest('dist/css'))
+});
